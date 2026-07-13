87fadff6f49a8ed36cf88dc67affdf91f0117b80
Aayam Neupane <aayamneupane@Mac.lan>
2026-07-13 14:59:48 -0400

    misc changes

--- src/main/java/com/project/taskmanagement/repository/TicketRepository.java
+++ src/main/java/com/project/taskmanagement/repository/TicketRepository.java
@@ -1,4 +1,12 @@
 package com.project.taskmanagement.repository;
 
-public class TicketRepository {
+import com.project.taskmanagement.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByTitleContainingIgnoreCase(String title);
+    long countByStatus(String status);
+    long countByPriority(String priority);
 }
