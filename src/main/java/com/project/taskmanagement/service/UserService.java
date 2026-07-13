b993ad452c36ab230decacfc8c591eee18239fd8
Aayam Neupane <aayamneupane@Mac.lan>
2026-07-13 14:58:47 -0400

    added User service for user logic

--- src/main/java/com/project/taskmanagement/service/UserService.java
+++ src/main/java/com/project/taskmanagement/service/UserService.java
@@ -1,4 +1,33 @@
 package com.project.taskmanagement.service;
 
+import com.project.taskmanagement.entity.User;
+import com.project.taskmanagement.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(user.getPassword())) {
+            return user;
+        }
+
+        return null;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
